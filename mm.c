0cee0d7d92ee5dfade00cf77bf3bacc7ee2d6f61
seonghoo1217 <lsh6451217@gmail.com>
2024-04-12 18:57:51 +0900

    refactor : modify team info in mm.c

--- mm.c
+++ mm.c
@@ -23,16 +23,16 @@
  * provide your team information in the following struct.
  ********************************************************/
 team_t team = {
-    /* Team name */
-    "ateam",
-    /* First member's full name */
-    "Harry Bovik",
-    /* First member's email address */
-    "bovik@cs.cmu.edu",
-    /* Second member's full name (leave blank if none) */
-    "",
-    /* Second member's email address (leave blank if none) */
-    ""
+        /* Team name */
+        "3team",
+        /* First member's full name */
+        "Seong Ho Lee ",
+        /* First member's email address */
+        "lsh6451217@gmail.com",
+        /* Second member's full name (leave blank if none) */
+        "Jae Hyeok Jeung",
+        /* Second member's email address (leave blank if none) */
+        "zaqokm2@gmail.com"
 };
 
 /* single word (4) or double word (8) alignment */
@@ -61,7 +61,7 @@ void *mm_malloc(size_t size)
     int newsize = ALIGN(size + SIZE_T_SIZE);
     void *p = mem_sbrk(newsize);
     if (p == (void *)-1)
-	return NULL;
+        return NULL;
     else {
         *(size_t *)p = size;
         return (void *)((char *)p + SIZE_T_SIZE);
@@ -83,13 +83,13 @@ void *mm_realloc(void *ptr, size_t size)
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
-    
+
     newptr = mm_malloc(size);
     if (newptr == NULL)
-      return NULL;
+        return NULL;
     copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
     if (size < copySize)
-      copySize = size;
+        copySize = size;
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
